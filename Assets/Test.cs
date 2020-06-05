087a28c582e5751d3aee40316bb651d9eb4f10a7
tama926 <acta_tamami@icloud.com>
2020-06-05 10:55:21 +0900

    ScriptTest　課題レビュー
    テスト

---
+++ Assets/Test.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        // 配列を初期化する
+        int[] array = { 30, 20, 50, 10, 80 };
+
+        // 配列の要素数のぶんだけ処理を繰り返す
+        for (int i = 0; i < array.Length; i++)
+
+
+            // 配列の要素を表示する
+            Debug.Log(array[i]);
+
+
+        for (int i = array.Length - 1; i >= 0; i--)
+
+
+            // 配列の要素を表示する
+            Debug.Log(array[i]);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
